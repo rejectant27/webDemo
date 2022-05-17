0cd79cf250924d23de18419f711c7d9169b9004c
1135186455@qq.com <1135186455@qq.com>
2022-05-17 09:47:53 +0800

    modify

--- src/main/java/com/example/webdemo/controller/TestController.java
+++ src/main/java/com/example/webdemo/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping
 public class TestController {
@@ -12,5 +14,10 @@ public class TestController {
     @GetMapping("get")
     public String whatYouType(@RequestParam("param") String param) {
         return "what you type is" + param;
+    }
+
+    @GetMapping()
+    public String firstPage() {
+        return "Greetings from Spring Boot " + LocalDateTime.now();
     }
 }
