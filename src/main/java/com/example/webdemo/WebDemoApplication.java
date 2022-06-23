1b8c9cbfab796afda42785bff8585dce0aa40701
1135186455@qq.com <1135186455@qq.com>
2022-06-23 21:25:25 +0800

    refer self maven repo

--- src/main/java/com/example/webdemo/WebDemoApplication.java
+++ src/main/java/com/example/webdemo/WebDemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.webdemo;
 
+import com.example.webdemo.service.Test;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebDemoApplication {
+public class WebDemoApplication implements InitializingBean {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WebDemoApplication.class, args);
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(Test.testHuToolUtil());
+        System.out.println(Test.getWhatType("dddddddddddddddddddddddddddddddddddd"));
+    }
 }
---
+++ src/main/java/com/example/webdemo/service/Test.java
@@ -0,0 +1,18 @@
+package com.example.webdemo.service;
+
+import cn.hutool.core.date.DateUtil;
+
+import java.time.LocalDateTime;
+
+public class Test {
+
+    public static String getWhatType(String str){
+        return "The content you type is " +str;
+    }
+
+    public static String testHuToolUtil(){
+//        DateUt
+        return DateUtil.format(LocalDateTime.now(),"current date is yyyy-MM-dd");
+    }
+
+}
